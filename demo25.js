b43934a05de5a67a330e61e1bf9ea3af4a0709dc
AsheshRanaG <ashesh.gurung@maitri.com>
2025-05-26 07:22:11 +0545

    feat: api mock test

--- demo25.js
+++ demo25.js
@@ -1,2 +1,27 @@
-console.log("project setup")
-console.log("login page feature")
+const UserData=[
+    {id:1,name:"abdul",gender:"male",age:20,occupation:undefined},
+    {id:2,name:"surendra",gender:"male",age:16,occupation:"intern"},
+    {id:3,name:"kavi",gender:"male",age:21,occupation:"streamer"},
+    {id:4,name:"sabina",gender:"female",age:18,occupation:undefined},
+    {id:5,name:"rabina",gender:"female",age:19,occupation:"SWE"},
+    {id:6,name:"tripal",gender:"male",age:25,occupation:undefined},
+
+]
+const response={
+    status:200,
+    message:"User data fetched successfully",
+    data:UserData
+}
+
+const fetchUserNames=()=>{
+    if(response.status ===200){
+        const result=response.data
+        .filter((user)=> user.occupation||user.age>20)
+        .map((user)=> user.name)
+        return result
+    }else{
+        const message="API fetch failed"
+        return message
+    }
+}
+console.log(fetchUserNames())
